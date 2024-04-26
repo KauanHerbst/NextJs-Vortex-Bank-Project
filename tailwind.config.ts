0f9111eeb27ac8071e387189383ac8a8bd654d56
KauanHerbst <kauanherbst7@gmail.com>
2024-04-26 00:45:54 -0300

    Created Project Setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,27 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'blue-primary': '#007BFF',
+        'blue-white-1': '#2D99FF',
+        'blue-white-2': '#59B5FF',
+        'blue-black-1': '#0062DC',
+        'blue-black-2': '#003596',
+        'purple-black-primary': '#0C0113',
+        'purple-black-1': '#0B0011',
+        'purple-black-2': '#0A000F',
+        'white-black-primary': '#E3E0E0',
+        'auxiliary-green': '#84F671',
+        'auxiliary-red': '#DA6E6E',
+        'gradient-blue-start': '#010231',
+        'gradient-blue-end': '#030697',
       },
     },
   },
