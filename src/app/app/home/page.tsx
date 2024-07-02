c0b8e1a8c8fe557ce2b329ab8f397ce95409ee44
KauanHerbst <kauanherbst7@gmail.com>
2024-07-02 15:33:43 -0300

    fix(app): fixes overview/home page

--- src/app/app/home/page.tsx
+++ src/app/app/home/page.tsx
@@ -5,13 +5,14 @@ import SupportCard from '@/components/app/SupportCard/SupportCard';
 
 export default function Home() {
   return (
-    <div className="w-[100%] h-[100%] flex justify-center items-center bg-white dark:bg-purple-black-2">
-      <div className="w-[1140px] flex justify-center gap-12 max-lg:gap-5 max-md:flex-col max-md:items-center">
+    <div className="w-[100%] h-[100%] flex md:justify-center md:items-center bg-white dark:bg-purple-black-2">
+      <div className="w-[1140px] flex sm:justify-center gap-12 max-lg:gap-5 max-md:flex-col max-md:items-center max-sm:mt-10 ">
         <div className="">
           <BalanceCard></BalanceCard>
           <ActivityCard></ActivityCard>
           <SupportCard></SupportCard>
         </div>
+
         <div className="mr-4 max-lg:mr-0">
           <OverviewMenuCard></OverviewMenuCard>
         </div>
